e63fcc30ea1bf5e6194ea8186403212306358b84
Cassandrea <cassssandrea@gmail.com>
2017-03-24 00:05:15 +0800

    part 5 labs added

--- part_05/Exercise_05.java
+++ part_05/Exercise_05.java
@@ -7,3 +7,15 @@ package part_05;
  * out "Hello Recursion!" 10 times.
  *
  */
+
+//class cookies {
+//    private String type;
+//    private int numberOfCookies;
+//    private double bites, cost;
+//    private boolean isChocolate, isSavory;
+//
+//    double cookieCost(int number, double cost) {
+//        cost = number*cost;
+//        return cost;
+//    }
+//}
--- part_05/Exercise_08.java
+++ part_05/Exercise_08.java
@@ -5,3 +5,15 @@ package part_05;
  * of the outer class. Also, within the inner class, call at least two methods that have a return type of int. Add these
  * two ints together and set a final instance variable in the outer with this value.
  */
+
+//class cookies {
+//    private String type;
+//    private int numberOfCookies;
+//    private double bites, cost;
+//    private boolean isChocolate, isSavory;
+//
+//    double cookieCost(int number, double cost) {
+//        cost = number*cost;
+//        return cost;
+//    }
+//}
